e606d81c648b13b1cd0b8b183205127d98486271
isoul87 <atbrxnk@gmail.com>
2025-10-12 13:57:40 +0700

    add filter feature

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,28 @@
+const btnFilter = document.querySelectorAll("#filterDvs ul li button");
+const imgItem  = document.querySelectorAll("#filterDvsImg .relative");
+
+
+btnFilter.forEach(data => {
+    data.onclick = () => {
+        btnFilter.forEach(data => {
+            data.classList.remove("btn-list-active")
+        });
+        data.classList.add("btn-list-active")
+
+        // filter images
+        const btnText = data.textContent;
+        imgItem.forEach(img => {
+            img.style.display = "none";
+            
+            if(img.getAttribute("data-filter") == btnText || btnText == "All"){
+                img.style.display ="block";
+            }
+        })
+    };
+});
+
+
+
 // const navBtn = document.getElementById("nav-btn");
 // const navCta = document.getElementById("navbar-cta")
 // const chdNavBtn = document.querySelectorAll("header nav div ul li a")
