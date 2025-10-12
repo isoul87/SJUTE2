74ed768712736c184e530aaf3e55796369bdf08b
isoul87 <atbrxnk@gmail.com>
2025-10-12 13:38:43 +0700

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,15 @@
+// const navBtn = document.getElementById("nav-btn");
+// const navCta = document.getElementById("navbar-cta")
+// const chdNavBtn = document.querySelectorAll("header nav div ul li a")
+
+// navBtn.addEventListener("click",() => {
+//   navCta.classList.toggle("hidden")
+//   navCta.classList.toggle("absolute")
+// });
+
+// chdNavBtn.forEach(btn => {
+//   btn.addEventListener("click", () => {
+//     [...chdNavBtn].forEach(el => el.classList.remove("activeNav"));
+//     btn.classList.add("activeNav");
+//   });
+// });
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+ /** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./index.html",
+    "./node_modules/flowbite/**/*.js"
+   ],
+   theme: {
+    fontFamily: {
+      'inter': ['Inter', 'ui-sans-serif'],
+      'Nunito': ['Nunito Sans', 'ui-sans-serif'],
+      'Mons': ['Montserrat', 'ui-sans-serif']
+    },
+     container : {
+       center: true,
+       padding: '16px',
+     },
+     extend: {
+    },
+  },
+   plugins: [
+    require('flowbite/plugin')
+   ],
+ }
